a5e1c716ae1fe18bbfdfa9a46bf43f070c4a75fa
LIPIVY <willc116@gmail.com>
2015-10-10 00:41:24 -0700

    Update globals.h

--- globals.h
+++ globals.h
@@ -8,10 +8,36 @@ namespace globals {
 	const float SPRITE_SCALE = 2.0f;
 }
 
+namespace sides {
+	enum Side {
+		TOP,
+		BOTTOM,
+		LEFT,
+		RIGHT,
+		NONE
+	};
+
+	inline Side getOppositeSide(Side side) {
+		return
+			side == TOP ? BOTTOM :
+			side == BOTTOM ? TOP :
+			side == LEFT ? RIGHT :
+			side == RIGHT ? LEFT :
+			NONE;
+	}
+}
+
+enum Direction {
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN
+};
+
 struct Vector2 {
 	int x, y;
 	Vector2() :
-		x(0), y(0)
+		x(0), y(0)	
 	{}
 	Vector2(int x, int y) :
 		x(x), y(y)
@@ -20,5 +46,6 @@ struct Vector2 {
 		return Vector2(0, 0);
 	}
 };
+
 
 #endif
